68db6368ffc008610832f0503ed19ed80e0729d3
Natalie Noemí Zamora Santizo <imachaoskawaii@gmail.com>
2021-04-26 08:56:35 -0600

    Update js.js

--- js/js.js
+++ js/js.js
@@ -2,34 +2,50 @@ var btr = document.getElementById("btn-r");
 var bts = document.getElementById("btn-s");
 var btm = document.getElementById("btn-m");
 var btd = document.getElementById("btn-d");
+var btnr = document.getElementById("btn-rr");
 
 
 function multiplica() {
     var num1 = document.getElementById("n1").value;
     var num2 = document.getElementById("n2").value;
-    alert(num1*num2)
+    var num3 = document.getElementById("result");
+    
+    num3.textContent=(num1*num2)
 }
 
 function division() {
     var num1 = document.getElementById("n1").value;
     var num2 = document.getElementById("n2").value;
-    alert(num1 / num2)
+    var num3 = document.getElementById("result");
+
+    num3.textContent=(num1 / num2)
 }
 
 function suma() {
     var num1 = document.getElementById("n1").value;
     var num2 = document.getElementById("n2").value;
-    alert(num1 + num2)
+    var num3 = document.getElementById("result");
+    
+    num3.textContent=(num1 + num2)
 }
 
 function resta() {
     var num1 = document.getElementById("n1").value;
     var num2 = document.getElementById("n2").value;
-    alert(num1 - num2)
+    var num3 = document.getElementById("result");
+    
+    num3.textContent=(num1 - num2)
     console.log("a")
+}
+
+function reset() {
+    var num3 = document.getElementById("result");
+    
+    num3.textContent=('0')
 }
 
 btm.addEventListener('click', multiplica);
 btd.addEventListener('click', division);
 bts.addEventListener('click', suma);
 btr.addEventListener('click', resta);
+btnr.addEventListener('click', reset);
